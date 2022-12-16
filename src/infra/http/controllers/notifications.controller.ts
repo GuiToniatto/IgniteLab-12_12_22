c0b76d4c8c2cd29a18ddcb0ea25b5e199ef990f7
Guilherme Toniatto Simoes <guilhermetoniatto@hotmail.com>
2022-12-16 11:02:29 -0300

    Finalização da aplicação

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,24 @@
+import { CancelNotification } from '@app/use-cases/Notification/CancelNotification';
+import { CountRecipientNotifications } from '@app/use-cases/Notification/CountRecipientNotifications';
+import { GetRecipientNotifications } from '@app/use-cases/Notification/GetRecipientNotifications';
+import { ReadNotification } from '@app/use-cases/Notification/ReadNotification';
+import { UnreadNotification } from '@app/use-cases/Notification/UnreadNotification';
 import { Body, Controller, Post } from '@nestjs/common';
+import { Get, Param, Patch } from '@nestjs/common/decorators';
 import { SendNotification } from 'src/app/use-cases/Notification/SendNotifications';
 import { CreateNotificationBody } from '../dtos/CreateNotificationBody';
 import { NotificationViewModel } from '../view-models/NotificationViewModel';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private sendNotification: SendNotification) {}
+  constructor(
+    private cancelNotification: CancelNotification,
+    private countRecipientNotifications: CountRecipientNotifications,
+    private getRecipientNotifications: GetRecipientNotifications,
+    private readNotification: ReadNotification,
+    private sendNotification: SendNotification,
+    private unreadNotification: UnreadNotification,
+  ) {}
 
   @Post()
   async create(@Body() body: CreateNotificationBody) {
@@ -22,5 +35,48 @@ export class NotificationsController {
     return {
       notification: raw,
     };
+  }
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotification.execute({
+      notificationId: id,
+    });
+  }
+
+  @Get('count/from/:recipientId')
+  async countFromRecipient(@Param('recipientId') recipientId: string) {
+    const { count } = await this.countRecipientNotifications.execute({
+      recipientId,
+    });
+
+    return {
+      count,
+    };
+  }
+
+  @Get('from/:recipientId')
+  async getFromRecipient(@Param('recipientId') recipientId: string) {
+    const { notifications } = await this.getRecipientNotifications.execute({
+      recipientId,
+    });
+
+    return {
+      notifications: notifications.map(NotificationViewModel.toHTTP),
+    };
+  }
+
+  @Patch(':id/read')
+  async read(@Param('id') id: string) {
+    await this.readNotification.execute({
+      notificationId: id,
+    });
+  }
+
+  @Patch(':id/unread')
+  async unread(@Param('id') id: string) {
+    await this.unreadNotification.execute({
+      notificationId: id,
+    });
   }
 }
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,3 +1,8 @@
+import { CancelNotification } from '@app/use-cases/Notification/CancelNotification';
+import { CountRecipientNotifications } from '@app/use-cases/Notification/CountRecipientNotifications';
+import { GetRecipientNotifications } from '@app/use-cases/Notification/GetRecipientNotifications';
+import { ReadNotification } from '@app/use-cases/Notification/ReadNotification';
+import { UnreadNotification } from '@app/use-cases/Notification/UnreadNotification';
 import { Module } from '@nestjs/common';
 import { SendNotification } from 'src/app/use-cases/Notification/SendNotifications';
 import { DatabaseModule } from '../database/database.module';
@@ -6,6 +11,13 @@ import { NotificationsController } from './controllers/notifications.controller'
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    SendNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
